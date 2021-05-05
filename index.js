f4949e4336e0274d4f2c211d63dd0f6f1820dcf1
Ryan Erricson <ryanerricson@gmail.com>
2021-05-05 13:55:27 -0400

    commit tree ae16159cddb7611e59033cb8cda07ea3ebcd30c6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const driver = {
+    name: 'Ryan',
+    address: 'Somewhere in Beantown'
+  };
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value
+    return driver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = Object.assign({}, driver);
+    delete newDriver[key];
+    return newDriver;
+}
